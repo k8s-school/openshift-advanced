b2cb1f5abc089a42fc641d6da2c753c6bab7ab2a
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-11-14 20:14:13 +0100

    Update current feature

--- bootstrap/1_addusers.sh
+++ bootstrap/1_addusers.sh
@@ -12,8 +12,8 @@ do
   echo $USER
   id -u $USER &>/dev/null || sudo useradd "$USER" --create-home --groups docker --shell /bin/bash
   echo "${USER}:${i}${PASS}" | sudo chpasswd
-  mkdir -p /home/$USER/.kube
-  mkdir -p /home/$USER/.ktbx/homefs
+  sudo su "$USER" -c "mkdir -p /home/$USER/.kube"
+  sudo su "$USER" -c "mkdir -p /home/$USER/.ktbx/homefs"
   chcon -Rt svirt_sandbox_file_t /home/$USER/.kube
   chcon -Rt svirt_sandbox_file_t /home/$USER/.ktbx/homefs
 done
