3649b8cc4dd8413a7e5b74de44905244171d33e9
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-11-19 10:57:38 +0100

    Update current feature

--- infra/az/scp-pull-secret.sh
+++ infra/az/scp-pull-secret.sh
@@ -1,1 +1,11 @@
-scp pull-secret root@163.172.132.135:/tmp
+#!/bin/bash
+
+set -euxo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+IP=$($DIR/../scw/get_ip.sh)
+
+# TODO move to right directory
+scp pull-secret root@"$IP":/tmp
+scp ~/.azure/osServicePrincipal.json root@"$IP"
---
+++ infra/scw/get_ip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Display the ssh command to connect to the instance
+
+set -euo pipefail
+
+instance_id=$(scw instance server list | grep openshift | awk '{print $1}')
+ip_address=$(scw instance server wait "$instance_id" | grep PublicIP.Address | awk '{print $2}')
+
+echo "$ip_address"
