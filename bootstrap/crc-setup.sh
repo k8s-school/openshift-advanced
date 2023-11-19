3827e45347803b86b6a5ac769eaa76a82d4e0df3
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-11-19 19:44:42 +0100

    Update current feature

--- bootstrap/crc-setup.sh
+++ bootstrap/crc-setup.sh
@@ -27,6 +27,7 @@ $crc config set memory 24576
 # Required on Fedora 38
 $crc config set skip-check-daemon-systemd-unit true
 $crc config set skip-check-daemon-systemd-sockets true
-# Enable cluster monitoring, this require at least 14 GiB of memory (a value of 14336)
-# $crc config set enable-cluster-monitoring true
+# Enable cluster monitoring if true
+# this require at least 14 GiB of memory (a value of 14336)
+$crc config set enable-cluster-monitoring false 
 $crc setup
