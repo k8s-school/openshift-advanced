4063effea6ab13f933111d692ffc29eb83b7c508
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-11-20 19:55:25 +0100

    Update current feature

--- labs/1_internals/ex1.sh
+++ labs/1_internals/ex1.sh
@@ -10,5 +10,9 @@ kubectl  wait --timeout=240s --for=condition=Ready -n "$ns" pods -l "app=etcd,et
 kubectl get pods -n "$ns"
 etcd_pod=$(kubectl get pods -n "$ns" -l "app=etcd,etcd=true,k8s-app=etcd" -o jsonpath='{.items[0].metadata.name}')
 
+# Display Kubernetes keys
+kubectl exec -t -n "$ns" "$etcd_pod" -- etcdctl get /kubernetes.io --keys-only --prefix
+
+# Display OpenShift keys (CustomResources)
 kubectl exec -t -n "$ns" "$etcd_pod" --  \
     sh -c "echo \$ETCDCTL_CERT \$ETCDCTL_KEY \$ETCDCTL_CACERT && etcdctl get /openshift.io --keys-only --prefix"
--- labs/2_authorization/README.md
+++ labs/2_authorization/README.md
@@ -1,8 +1,3 @@
-# Pre-requisite
+# Openshift advanced labs
 
-Run on kind cluster >= v0.5.1
-
-# Create user
-
-Documentation:
-https://docs.bitnami.com/kubernetes/how-to/configure-rbac-in-your-kubernetes-cluster/#use-case-1-create-user-with-limited-namespace-access
+Labs solution are available (here)[labs].
