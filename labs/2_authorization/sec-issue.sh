bfa2d2b61b4a2aa3d3b1c0a40d3c5e4cb62ee6e9
Fabrice Jammes <fjammes@users.noreply.github.com>
2023-11-23 20:07:23 +0100

    Update sec-issue.sh

--- labs/2_authorization/sec-issue.sh
+++ labs/2_authorization/sec-issue.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Inject a security issue inside a k8s cluster
+
 set -euxo pipefail
 
 kubectl create namespace monitoring
 kubectl create clusterrolebinding cluster-monitoring --clusterrole=cluster-admin --serviceaccount=monitoring:default
 
-
 # To find it use "rbac-tool analysis", and then
+kubens monitoring
+SERVICE_ACCOUNT_NAME="default"
 kubectl get rolebinding,clusterrolebinding --all-namespaces -o jsonpath="{range .items[?(@.subjects[0].name=='$SERVICE_ACCOUNT_NAME')]}[role: {.roleRef.kind},{.roleRef.name}, binding: {.metadata.name}]{end}"
 # it will output:
 # [role: ClusterRole,cluster-admin, binding: cluster-monitoring]
