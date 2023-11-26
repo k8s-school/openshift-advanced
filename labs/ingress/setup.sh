4e9778dbf5e47be64a94426737db53537e22901d
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-11-26 18:44:41 +0100

    Update current feature

--- labs/ingress/setup.sh
+++ labs/ingress/setup.sh
@@ -2,7 +2,7 @@
 
 # See https://kubernetes.github.io/ingress-nginx/deploy/#quick-start
 # and https://kubernetes.io/docs/tasks/access-application-cluster/ingress-minikube/
-set -euxo pipefail
+set -euo pipefail
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 
@@ -11,23 +11,28 @@ NSAPP="ingress-app"
 
 # Run on kubeadm cluster
 # see "kubernetes in action" p391
-kubectl delete project -l "ingress=nginx"
+kubectl delete project -l "kubernetes.io/metadata.name: $NSAPP"
 oc new-project "$NSAPP"
-kubectl label ns "$NSAPP" "ingress=nginx"
 
-# Deploy application
+ink "Deploy application"
+set -x
 kubectl create deployment web -n "$NSAPP" --image=gcr.io/google-samples/hello-app:1.0
 kubectl expose deployment web -n "$NSAPP" --port=8080
 kubectl  wait -n "$NSAPP" --for=condition=available deployment web
 
-# Create ingress route
+set +x
+ink "Create ingress route"
+set -x
 kubectl apply -n "$NSAPP" -f $DIR/example-ingress.yaml
 kubectl get -n "$NSAPP" ingress
 
-ink "INFO: Add application URL to DNS"
+set +x
+ink "INFO: Add application URL to DNS (/etc/hosts hack))"
 export server_ip=$(ip add show eth0 | grep -w inet | awk '{print $2;exit}' | cut -d'/' -f1)
 go install github.com/txn2/txeh/txeh@v1.5.4
 sudo $(which txeh) add "$server_ip" hello-world.info
+cat /etc/hosts | grep hello-world.info
 
-ink "INFO: access the application via ingress"
+ink "access the application via ingress"
+set -x
 curl -k https://hello-world.info
