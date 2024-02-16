3dd221f625735946b6b7e9809dc82d39f59c6ad5
Your Name <you@example.com>
2024-02-16 11:37:03 +0100

    Update current feature

--- labs/3_policies/ex3-scc.sh
+++ labs/3_policies/ex3-scc.sh
@@ -16,23 +16,22 @@ ID="$(whoami)"
 NS="scc-$ID"
 SA="fake-user"
 
-# See https://kubernetes.io/docs/concepts/policy/pod-security-policy/#run-another-pod
-ink "Reset scc namespace $NS and remove related scc"
-kubectl delete namespace -l "scc=$ID"
-oc adm policy remove-scc-from-user anyuid -z $SA || ink -y "WARN: anyuid not allowed to $SA"
-oc adm policy remove-scc-from-user hostpath-provisioner -z $SA || ink -y "WARN: hostpath-provisioner not allowed to $SA"
-oc adm policy remove-scc-from-user hostpath-provisioner -z default || ink -y "WARN: hostpath-provisioner not allowed to default"
-
-ink "Create namespace $NS and service account $SA"
-oc new-project "$NS"
-kubectl label ns "$NS" "scc=$ID"
-kubectl create serviceaccount -n "$NS" $SA
-
 # WARN alias does not work with bash
 shopt -s expand_aliases
 alias kubectl-admin='kubectl -n "$NS"'
 alias kubectl-user='kubectl --as=system:serviceaccount:$NS:$SA -n "$NS"'
 
+# See https://kubernetes.io/docs/concepts/policy/pod-security-policy/#run-another-pod
+ink "Reset scc namespace $NS and remove related scc"
+oc adm policy remove-scc-from-user anyuid -z $SA || ink -y "WARN: anyuid not allowed to $SA"
+oc adm policy remove-scc-from-user hostpath-provisioner -z $SA || ink -y "WARN: hostpath-provisioner not allowed to $SA"
+oc adm policy remove-scc-from-user hostpath-provisioner -z default || ink -y "WARN: hostpath-provisioner not allowed to default"
+kubectl delete namespace -l "scc=$ID"
+
+ink "Create namespace $NS and service account $SA"
+oc new-project "$NS"
+kubectl label ns "$NS" "scc=$ID"
+kubectl create serviceaccount -n "$NS" $SA
 
 if [ "$EX3_SCC_FULL" = false ]
 then
@@ -47,13 +46,12 @@ kubectl-user create -f $tmp_dir/ubuntu-simple.yaml
 
 ink "Check access to scc"
 kubectl --as=system:serviceaccount:"$NS":$SA  auth can-i use scc/restricted-v2
+kubectl --as=system:serviceaccount:"$NS":$SA  auth can-i use scc/anyuid
 
 ink "Check which scc was used"
-kubectl get pod ubuntu-simple -o yaml | grep -i runasuser
 kubectl get pod ubuntu-simple -o yaml | grep -i scc
 
 ink "Show which SCC is required by the pod ubuntu-simple"
-ink "WARN: command should not display anyuid here: inconsistency"
 oc adm policy scc-subject-review -f $tmp_dir/ubuntu-simple.yaml
 
 ink "Try to create pod ubuntu-root"
