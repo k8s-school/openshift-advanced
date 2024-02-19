1d37017bdf8178cabb12917b5fabe1bb6e49e1b2
Your Name <you@example.com>
2024-02-19 23:06:30 +0100

    Update current feature

--- infra/az/scp-pull-secret.sh
+++ infra/az/scp-pull-secret.sh
@@ -6,10 +6,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 IP=$($DIR/../scw/get_ip.sh)
 
-# TODO move to right directory
 scp pull-secret root@"$IP":/tmp
 scp ~/.azure/osServicePrincipal.json root@"$IP":/tmp
 
-# TODO
-ssh root@"$IP" sh -c "chown openshift:openshift /tmp/pull-secret && sudo su openshift && mv /tmp/pull-secret /home/openshift/openshift-advanced/"
-ssh root@"$IP" sh -c "chown openshift:openshift /tmp/osServicePrincipal.json && sudo su openshift && mkdir -p /home/openshift/.azure && mv /tmp/osServicePrincipal.json /home/openshift/.azure"
+ssh root@"$IP" mv /tmp/pull-secret /home/openshift/openshift-advanced/infra/az
+ssh root@"$IP" "mkdir -p /home/openshift/.azure && mv /tmp/osServicePrincipal.json /home/openshift/.azure"
+
+ssh root@"$IP" chown -R openshift:openshift /home/openshift/openshift-advanced/infra/az/pull-secret
+ssh root@"$IP" chown -R openshift:openshift /home/openshift/.azure
