c02fc46c179f24d1c8d6be0127406883e314e562
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-10-11 21:52:06 +0200

    Update current feature

--- labs/run-all.sh
+++ labs/run-all.sh
@@ -6,12 +6,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ink "Run test on internals"
 "$DIR"/1_internals/ci.sh
-echo
+ink "Success"
 ink "Run test on authorization"
 "$DIR"/2_authorization/ci.sh
-echo
+ink "Success"
 ink "Run test on policies"
 "$DIR"/3_policies/ci-openshift.sh
-echo
+ink "Success"
 # ink "Run test on computational resources"
 # "$DIR"/4_computational_resources/ci.sh
