107ea58151a912e2ce85100f838384413727bd9f
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2025-03-26 12:36:58 +0100

    Update current feature

--- labs/3_policies/ex2-podsecurity.sh
+++ labs/3_policies/ex2-podsecurity.sh
@@ -224,10 +224,9 @@ else
     exit 1
 fi
 
-ink "Use 'docker exec -it <node-name> -- bash and crictl inspect' to check pods on nodes"
-
-node=$(kubectl get pod -n verify-pod-security ubuntu-restricted -o "jsonpath={.spec.nodeName}")
-docker exec -it $node crictl inspect ubuntu
+# ink "Use 'docker exec -it <node-name> -- bash and crictl inspect' to check pods on nodes"
+# node=$(kubectl get pod -n verify-pod-security ubuntu-restricted -o "jsonpath={.spec.nodeName}")
+# docker exec -it $node crictl inspect ubuntu
 
 
 
